52152a0fb660da5703a0655608fe0bbbebfd44de
andreea <nicolaeioanscortaru@gmail.com>
2024-04-24 22:43:29 +0300

    setup react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import './App.css'
+import { Outlet } from "react-router-dom";
+import { Container } from 'react-bootstrap';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Header from "./components/Header/Header"
+
 
 function App() {
   return (
-    <>auth</>
+    <>
+      <Container className='my-2'>
+        <ToastContainer />
+        <Outlet />
+      </Container>
+    </>
   )
 }
 
-export default App;
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider
+} from 'react-router-dom';
+
+import App from './App';
+
+import HomePage from './pages/HomePage/HomePage';
+import LoginPage from './pages/Login/LoginPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />} >
+      <Route index={true} path='/' element={<HomePage />} />
+      <Route path='/login' element={<LoginPage />} />
+    </Route>
+  ));
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
