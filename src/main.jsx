5739e43b571a3d01df62af7ee369e40aa4023101
andreea <nicolaeioanscortaru@gmail.com>
2024-04-25 00:06:50 +0300

    add store

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,10 @@ import {
   Route,
   RouterProvider
 } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import App from './App';
-
+import store from './store';
 import HomePage from './pages/Home/HomePage';
 import LoginPage from './pages/Login/LoginPage';
 import UserDetails from './pages/UserDetails/UserDetails'
@@ -20,12 +21,14 @@ const router = createBrowserRouter(
     <Route path='/' element={<App />} >
       <Route index={true} path='/' element={<HomePage />} />
       <Route path='/login' element={<LoginPage />} />
-      <Route path='/user-details' element={<UserDetails/>}/>
+      <Route path='/user-details' element={<UserDetails />} />
     </Route>
   ));
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </Provider>,
 )
