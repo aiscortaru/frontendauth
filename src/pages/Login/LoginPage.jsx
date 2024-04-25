dea4efff8672874e6f9c05de15e8d00371ddfcd2
andreea <nicolaeioanscortaru@gmail.com>
2024-04-25 21:47:09 +0300

    login functionality

--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -1,17 +1,36 @@
 import React from 'react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import { Form, Button } from 'react-bootstrap';
+import { toast } from 'react-toastify';
+import { useLoginMutation } from '../../slices/userApiSlice.js';
 import FormContainer from '../../components/LoginContainer/FormContainer.jsx';
-
 
 const LoginPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [login, { isLoading }] = useLoginMutation();
+  const { userInfo } = useSelector((state) => state.auth);
 
-  const submitHandler = async (e) => {
-    e.preventDefault();
-    console.log(email,password);
-  }
+  useEffect(() => {
+      if (userInfo) {
+        navigate('/');
+      }
+    }, [navigate, userInfo]);
+  
+    const submitHandler = async (e) => {
+      e.preventDefault();
+      try {
+        const res = await login({ email, password }).unwrap();
+        dispatch(setCredentials({ ...res }));
+        navigate('/');
+      } catch (err) {
+        toast.error(err?.data?.message || err.error);
+      }
+    };
 
   return (
     <FormContainer>
