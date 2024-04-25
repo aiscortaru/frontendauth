a016498f51f650fe07c71de131e7bdd4b6dfe0f0
andreea <nicolaeioanscortaru@gmail.com>
2024-04-25 21:35:09 +0300

    adding header in App and rename usersApiSlice to userApiSlice

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Header from "./components/Header/Header"
 function App() {
   return (
     <>
+      <Header />
       <Container className='my-2'>
         <ToastContainer />
         <Outlet />
