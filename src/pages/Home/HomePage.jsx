88ae9a7bc0285704f7be7bdc0d9e73d4601c72f7
andreea <nicolaeioanscortaru@gmail.com>
2024-04-25 22:38:26 +0300

    adding home background design

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Image from 'react-bootstrap/Image';
+
 
 const HomePage = () => {
   return (
-    <div>
-      HomePage
+    <div style={{
+      backgroundImage:`url(https://www.ing.com/upload/35155fe9-8dfb-4ca3-a51e-104d116f01db_Welcome-_700x394.jpg)`,
+      height: '80vh'
+    }}> 
     </div>
   )
 }
