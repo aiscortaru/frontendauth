4d44cf38f97c7a9880cde0fa7a5ec813cb61df11
andreea <nicolaeioanscortaru@gmail.com>
2024-04-25 20:53:28 +0300

    add form login structure

--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -1,10 +1,47 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
+import FormContainer from '../../components/LoginContainer/FormContainer.jsx';
+
 
 const LoginPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    console.log(email,password);
+  }
+
   return (
-    <div>
-      LoginPage
-    </div>
+    <FormContainer>
+      <Form onSubmit={submitHandler}>
+        <Form.Group className='my-2' controlId='email'>
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type='email'
+            placeholder='Enter email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group className='my-2' controlId='password'>
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type='password'
+            placeholder='Enter password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Button type='submit' variant='primary' className='mt-3'>
+          Sign In
+        </Button>
+
+      </Form>
+    </FormContainer>
   )
 }
 
