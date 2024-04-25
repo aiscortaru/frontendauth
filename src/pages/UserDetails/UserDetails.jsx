675e037661fa77b46aa06142b0d52662223a310d
andreea <nicolaeioanscortaru@gmail.com>
2024-04-25 23:12:36 +0300

    add user details page

--- src/pages/UserDetails/UserDetails.jsx
+++ src/pages/UserDetails/UserDetails.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 const UserDetails = () => {
-  return (
-    <div>
-      User details
-    </div>
-  )
+    const { userInfo } = useSelector((state) => state.auth);
+    const {name, email, _id} = userInfo;
+
+    return (
+        <ListGroup>
+            <ListGroup.Item>Name: {name} </ListGroup.Item>
+            <ListGroup.Item>Email: {email}</ListGroup.Item>
+            <ListGroup.Item>Id: {_id}</ListGroup.Item>
+        </ListGroup>
+    )
 }
 
-export default UserDetails
+export default UserDetails;
