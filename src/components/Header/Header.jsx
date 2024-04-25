efa6cb55978a787b2d006945a73ec40c07122635
andreea <nicolaeioanscortaru@gmail.com>
2024-04-25 22:02:55 +0300

    add header functionality

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,71 @@
-import React from 'react'
+import { Navbar, Nav, Container, Button } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { FaSignInAlt, FaUserAstronaut, FaSignOutAlt } from 'react-icons/fa';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { useLogoutMutation } from '../../slices/userApiSlice.js';
+import { clearCredentials } from '../../slices/authSlice.js';
 
 const Header = () => {
+  const { userInfo } = useSelector((state) => state.auth);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [logoutApiCall] = useLogoutMutation();
+
+  const logoutHandler = async () => {
+    try {
+      await logoutApiCall().unwrap();
+      dispatch(clearCredentials());
+      navigate('/login');
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  console.log(userInfo)
+
   return (
-    <div>
-      Header Component
-    </div>
-  )
-}
+    <header>
+      <Navbar bg='light' variant='light' expand='lg' collapseOnSelect>
+        <Container>
+          <LinkContainer to='/'>
+            <Navbar.Brand>ING Auth</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ms-auto'>
+
+              {
+                userInfo ? (
+                  <>
+                    <LinkContainer to='/profile'>
+                      <Nav.Link>
+                        <Button variant="link"><FaUserAstronaut /> Profile</Button>
+                      </Nav.Link>
+                    </LinkContainer>
+                    { }
+                    <Button variant="link" onClick={logoutHandler}><FaSignOutAlt /> Sign out</Button>
+                  </>
+                ) : (
+                  <LinkContainer to='/login'>
+                    <Nav.Link>
+                      {
+                        !window.location.href.includes('login') ?
+                          <Button variant="link" onClick={logoutHandler}><FaSignInAlt /> Sign in</Button>
+                          : null
+                      }
+                    </Nav.Link>
+                  </LinkContainer>
+                )
+              }
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
 
 export default Header;
